d8ad91d9f28125dc93e304a7a7a7dc422210dbbd
martins.kuznecovs <martins.kuznecovs@gmail.com>
2024-12-20 17:19:49 +0200

    run local model in tensor parallel fashion

---
+++ run_test_accelerate_local_model.sh
@@ -0,0 +1,6 @@
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 lm_eval --model hf \
+    --model_args "pretrained=/tmp/martins/llm_dir/full_test_hf,parallelize=True" \
+    --tasks hellaswagx_lv \
+    --num_fewshot 10 \
+    --batch_size 2 \
+    --limit 100
