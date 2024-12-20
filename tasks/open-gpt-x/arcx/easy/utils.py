58530a9c9d91e8aa3e1246d80b553a1592657106
martins.kuznecovs <martins.kuznecovs@gmail.com>
2024-12-20 14:32:54 +0200

    add arcx bench

---
+++ tasks/open-gpt-x/arcx/easy/utils.py
@@ -0,0 +1,30 @@
+import re
+
+import datasets
+
+
+def preprocess(text):
+    text = text.strip()
+    text = text.replace(" [title]", ". ")
+    text = re.sub("\\[.*?\\]", "", text)
+    text = text.replace("  ", " ")
+    return text
+
+
+def process_docs(dataset: datasets.Dataset) -> datasets.Dataset:
+    def _process_doc(doc):
+        # breakpoint()
+        print(doc)
+        out_doc = {
+            "id": doc["id"],
+            "query": "Question: " + preprocess(doc["question"]) + "\nAnswer:",
+            "choices": [
+                preprocess(option)
+                for option in doc["choices"]["text"]
+                if option
+            ],
+            "gold": doc["choices"]["label"].index(doc["answerKey"]),
+        }
+        return out_doc
+
+    return dataset.map(_process_doc)
