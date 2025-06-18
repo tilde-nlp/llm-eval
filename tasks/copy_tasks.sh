f54513c66609daede349dac6300d8f83e274ef98
Jeļizaveta Jelinska <jelizaveta.jelinska@Tilde.lv>
2025-06-18 13:43:38 +0300

    Add Lithuanian language tasks from Neurotechnology

--- tasks/copy_tasks.sh
+++ tasks/copy_tasks.sh
@@ -1,2 +1,4 @@
 cp -r open-gpt-x ../lm-evaluation-harness/lm_eval/tasks/open-gpt-x
 cp -r tildelm ../lm-evaluation-harness/lm_eval/tasks/tildelm
+cp -r neurotechnology ../lm-evaluation-harness/lm_eval/tasks/neurotechnology
+cp -r SQuAD ../lm-evaluation-harness/lm_eval/tasks/SQuAD
