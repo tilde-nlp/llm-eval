f28cf813364703af1a1843035264f13f5ed717b4
jelizaveta.jelinska <lizavetakli@gmail.com>
2025-07-18 15:28:17 +0300

    Change national exams choices list

--- tasks/National_Exams/utils.py
+++ tasks/National_Exams/utils.py
@@ -3,21 +3,17 @@ def doc_to_text(doc):
     for i in range(len(doc["question"]["choices"]["text"])):
         label = doc["question"]["choices"]["label"][i]
         text = doc["question"]["choices"]["text"][i]
-        para = doc["question"]["choices"]["para"][i]
-        OUTPUT.append(f"{label}: {text}\n{para}\n")
-
-    # Join all choices and prepend the stem
+        OUTPUT.append(f"{label}: {text}")
     return f"{doc['question']['stem']}\n\n" + "\n".join(OUTPUT)
 
 
 def doc_to_choice(doc):
-    OUTPUT = []
-    for i in range(len(doc["question"]["choices"]["text"])):
-        label = doc["question"]["choices"]["label"][i]
-        text = doc["question"]["choices"]["text"][i]
-        para = doc["question"]["choices"]["para"][i]
-        OUTPUT.append(f"{label}: {text}\n{para}\n\n")
-    return OUTPUT
+    #OUTPUT = []
+    #for i in range(len(doc["question"]["choices"]["text"])):
+        #label = doc["question"]["choices"]["label"][i]
+        #text = doc["question"]["choices"]["text"][i]
+        #OUTPUT.append(f"{label}: {text}\n{para}\n\n")
+    return ["A", "B", "C", "D"]
 
 
 def doc_to_target(doc):
