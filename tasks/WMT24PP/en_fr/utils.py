b437a2fa5a2f6c8a0c9b018d1bcc7c6a4701ea71
jelizaveta.jelinska <lizavetakli@gmail.com>
2025-07-25 13:57:53 +0300

    updated utils and yamls

--- tasks/WMT24PP/en_fr/utils.py
+++ tasks/WMT24PP/en_fr/utils.py
@@ -1,33 +1,15 @@
 
-from sacrebleu.metrics import BLEU
-import os
-os.environ["HF_HUB_DISABLE_SYMLINKS_WINDOWS"] = "1"
+from datasets import Dataset
 
-bleu = BLEU()
-
-def clean_doc(doc):
-    # Remove auto-generated pandas index column if it exists
-    if '__index_level_0__' in doc:
-        del doc['__index_level_0__']
-    return doc
+def filter_data(dataset):
+    return dataset.filter(lambda doc: not doc.get('is_bad_source', False))
 
 def doc_to_text(doc):
-    doc = clean_doc(doc)
     docs = doc['source']
-    return (f"Translate the text given in English to French:
-"
-            f"{docs}
-
-")
+    return (f"Translate the text given in English to French:"
+            f"\n"
+            f"{docs}"
+            f"\n\n")
 
 def doc_to_target(doc):
-    doc = clean_doc(doc)
     return doc['target']
-
-def process_results(doc, results):
-    doc = clean_doc(doc)
-    prediction = results[0]
-    reference = doc_to_target(doc)
-    score = bleu.sentence_score(prediction.strip(), [reference.strip()]).score
-    return score
-
