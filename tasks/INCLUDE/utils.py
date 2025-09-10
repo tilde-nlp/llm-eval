f6466164afb3c7fa144615f76dc959525500263c
Martins Kronis <martins.kuznecovs@tilde.lv>
2025-09-10 14:45:50 +0300

    add multiple choice options to few shot examples

--- tasks/INCLUDE/utils.py
+++ tasks/INCLUDE/utils.py
@@ -1,10 +1,10 @@
 
 def doc_to_text(doc):
-    return doc['question']
+    return f"{doc['question']}\n{doc['option_a']}\n{doc['option_b']}\n{doc['option_c']}\n{doc['option_d']}\n\n"
 
 def doc_to_choice(doc):
-    return [doc['option_a'], doc['option_b'], doc['option_c'], doc['option_d'] ]
+    return [doc['option_a'], doc['option_b'], doc['option_c'], doc['option_d']]
 
 def doc_to_target(doc):
-    m = [doc['option_a'], doc['option_b'], doc['option_c'], doc['option_d'] ]
+    m = [doc['option_a'], doc['option_b'], doc['option_c'], doc['option_d']]
     return m[doc['answer']]
