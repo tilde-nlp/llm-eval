baf14b4e7295fe9dab04639884591bab88b1f66a
Martins Kronis <martins.kuznecovs@tilde.lv>
2025-09-22 14:45:04 +0300

    correct answer contains full response

--- tasks/LV_exams/utils.py
+++ tasks/LV_exams/utils.py
@@ -1,2 +1,21 @@
+letters = list(map(chr, range(ord('A'), ord('Z')+1)))
+
 def doc_to_text(doc):
-    return f"{doc["context"]}\n{doc["question"]}"
+    question = doc["question"]
+    ctx = doc["context"]
+    
+    answers = ""
+    for n, ans in enumerate(doc["options"]):
+        answers += f"{letters[n]}) {ans}\n"
+
+    answers += "\nAtbilde:"
+
+    return f"{question}\n{ctx}\n{answers}"
+
+
+def doc_to_choice(doc):
+    return [f"{letters[n]}) {ans}" for n, ans in enumerate(doc["options"])]
+
+
+def doc_to_target(doc):
+    return f"{letters[doc['correct_answer_index']]}) {doc['correct_answer_text']}"
